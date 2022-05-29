48065dd57476446fe62f0c3fc8a8929decbbb72e
Jônathas Bonfim <58302084+Jonathas-bonfim@users.noreply.github.com>
2022-05-29 12:36:33 -0300

    Structured project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import arrow from '../../assets/images/arrow.svg';
+import logo from '../../assets/images/logo-xbox.svg';
+import { Footer } from '../../components/footer';
+import { Header } from '../../components/header';
+
+export function Home(){
+    return(
+        <>
+            <Header />
+            <Footer />
+        </>
+    );
+}
